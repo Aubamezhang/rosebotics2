a239b8fbf02156a3167cbdc048e1c0a6566ac194
ketchaam <ketchaam@rose-hulman.edu>
2018-10-25 11:27:13 -0400

    test ketchaam.py

--- src/ketchaam.py
+++ src/ketchaam.py
@@ -10,7 +10,7 @@ import time
 def main():
     """ Runs YOUR specific part of the project """
     robot = rb.Snatch3rRobot()
-    robot.drive_system.go_straight_inches(4, 100, rb.StopAction.BRAKE)
+    robot.drive_system.go_straight_inches(8, 100, rb.StopAction.BRAKE)
 
 #er
 main()
