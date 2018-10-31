40f106d94b4eb225242db201eac489ab6695fdfe
Samuel D <dickinsw@rose-hulman.edu>
2018-10-30 20:56:37 -0400

    forgot to commit test cases

--- src/samuelwd.py
+++ src/samuelwd.py
@@ -9,7 +9,17 @@ import time
 
 def main():
     """ Runs YOUR specific part of the project """
+    run_test_spin_degrees()
+    run_test_turn_degrees()
+
+def run_test_spin_degrees():
     robot = rb.Snatch3rRobot()
-    robot.drive_system.spin_in_place_degrees(90, 100)
+    robot.drive_system.spin_in_place_degrees(45)
+    robot.drive_system.spin_in_place_degrees(-90)
+
+def run_test_turn_degrees():
+    robot = rb.Snatch3rRobot()
+    robot.drive_system.turn_degrees(90)
+    robot.drive_system.turn_degrees(-90)
 
 main()
