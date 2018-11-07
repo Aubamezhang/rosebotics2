50020cb039531f26512590d9e6e821fb5d564cd8
Samuel D <dickinsw@rose-hulman.edu>
2018-11-07 10:57:53 -0500

    my capstone

--- src/Jake_Zhang.py
+++ src/Jake_Zhang.py
@@ -35,14 +35,14 @@ def run_test_detect_colors():
     if robot.color_sensor.wait_until_color_is(color) == True:
         robot.drive_system.stop_moving()
 
-
+'''
 def run_test_ring():
     robot = rb.Snatch3rRobot()
     while True:
         robot.drive_system.start_moving(30, 30)
         if robot.color_sensor.wait_until_intensity_is_greater_than(10) == True:
             robot.drive_system.spin_in_place_degrees(-50, 100)
-
+'''
 
 def proximity_sensor(inches_low, inches_high):
     robot = rbnew.Snatch3rRobot()
--- src/samuelwd.py
+++ src/samuelwd.py
@@ -9,6 +9,9 @@ import time
 
 def main():
     """ Runs YOUR specific part of the project """
+    run_test_turn_degrees()
+    run_test_spin_degrees()
+    run_test_ring()
     run_test_ArmAndClaw()
 
 def run_test_spin_degrees():
@@ -24,6 +27,13 @@ def run_test_turn_degrees():
 def run_test_ArmAndClaw():
     robot = rb.Snatch3rRobot()
     robot.arm.raise_arm_and_close_claw(duty_cycle_percent=100)
-    #robot.arm.calibrate()
-    #robot.arm.move_arm_to_position(360 * 6.2) #postion in degrees
+    robot.arm.calibrate()
+    robot.arm.move_arm_to_position(360 * 6.2) #postion in degrees
+
+def run_test_ring():
+    robot = rb.Snatch3rRobot()
+    while True:
+        robot.drive_system.start_moving(30, 30)
+        if robot.color_sensor.wait_until_intensity_is_greater_than(10) == True:
+            robot.drive_system.spin_in_place_degrees(-50, 100)
 main()
