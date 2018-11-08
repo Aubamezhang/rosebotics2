f6ff79ce046519cadb96f1ec4609f9a5c471b54b
ketchaam <ketchaam@rose-hulman.edu>
2018-11-08 11:18:07 -0500

    done

--- src/ketchaam.py
+++ src/ketchaam.py
@@ -9,10 +9,10 @@ import ev3dev.ev3 as ev3
 
 def main():
     """ Runs YOUR specific part of the project """
-    run_test_go_straight_inches()
-    run_test_turn_degrees()
+    # run_test_go_straight_inches()
+    # run_test_turn_degrees()
     # run_test_spin_degrees()
-    # beep_if_blob_is_bigger_than(2000)
+    beep_if_blob_is_bigger_than(3000)
 
 def run_test_go_straight_inches():
     robot = rb.Snatch3rRobot()
@@ -21,13 +21,11 @@ def run_test_go_straight_inches():
 
 def run_test_spin_degrees():
     robot = rb.Snatch3rRobot()
-    robot.drive_system.spin_in_place_degrees(30)
     robot.drive_system.spin_in_place_degrees(-180)
 
 def run_test_turn_degrees():
     robot = rb.Snatch3rRobot()
     robot.drive_system.turn_degrees(45)
-    robot.drive_system.turn_degrees(-45)
 
 def beep_if_blob_is_bigger_than(area):
     robot = rb.Snatch3rRobot()
