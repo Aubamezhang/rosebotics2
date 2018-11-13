0ae2288cb2277c269ca03f56faeb4f4eea264650
zhangjl <zhangjl@rose-hulman.edu>
2018-11-13 17:14:47 -0500

    Working on Individual Project

--- src/zhang_runs_on_robot.py
+++ src/zhang_runs_on_robot.py
@@ -16,11 +16,13 @@ import mqtt_remote_method_calls as com
 import ev3dev.ev3 as ev3
 import random
 
+
 def main():
 
     robot = rb.Snatch3rRobot()
     ir_sensor = robot.proximity_sensor
-    rc = RemoteControlEtc(robot, ir_sensor, ev3)
+    arm_and_claw = rb.ArmAndClaw(touch_sensor=1)
+    rc = RemoteControlEtc(robot, ir_sensor, ev3, arm_and_claw)
     mqtt_client = com.MqttClient(rc)
     mqtt_client.connect_to_pc()
 
@@ -33,10 +35,11 @@ def main():
 
 
 class RemoteControlEtc(object):
-    def __init__(self, robot, ir_sensor, ev3):
+    def __init__(self, robot, ir_sensor, ev3, arm_and_claw):
         self.robot = robot
         self.proximity = ir_sensor
         self.ev3 = ev3
+        self.arm = arm_and_claw
         """
         Store the robot.
             :type robot: rb.Snatch3rRobot
@@ -56,16 +59,18 @@ class RemoteControlEtc(object):
 
     def route1(self):
         print('Robot will move in straight route')
+        self.robot.arm.calibrate()
         self.robot.drive_system.move_for_seconds(5, 75, 75)
+        print(self.proximity.get_distance_to_nearest_object_in_inches)
+        if self.proximity.get_distance_to_nearest_object_in_inches() <= 2:
+            if self.proximity.get_distance_to_nearest_object_in_inches() >= 0:
+                self.arm.raise_arm_and_close_claw()
         self.robot.drive_system.spin_in_place_degrees(180, 75, 75)
         self.robot.drive_system.move_for_seconds(5, 75, 75)
-        if self.proximity.get_distance_to_nearest_object_in_inches() <= 8:
-            if self.proximity.get_distance_to_nearest_object_in_inches() >= 3:
-                self.robot.drive_system.go_straight_inches(self.proximity.get_distance_to_nearest_object_in_inches() - 1)
-
 
     def route2(self):
         print('Robot will move in circular route')
+        self.arm.calibrate()
         rng_loop = random.randint(1, 5)
         for x in range(rng_loop):
             rng_speed = random.randint(50, 100)
@@ -76,9 +81,8 @@ class RemoteControlEtc(object):
 
 
 
-        # robot will drive a certain distance, if any object is detected, it will check the object and determine
-        # whether or not it is trash. If it is trash, the robot will pick it up and take it back to start, if not, the
-        # robot will pick it up and move it out of the way.
+        # robot will drive a certain distance, if any object is detected, it will be determined as trash and will carry
+        # it back to the starting point
 
 
 
